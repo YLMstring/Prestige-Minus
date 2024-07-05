f55692ad4179ec0c629b2614a00ee4b7401521e5
YLMstring <61271096+YLMstring@users.noreply.github.com>
2024-07-05 13:21:10 +0800

    Create FixRespec.cs

---
+++ PrestigeMinus/FixRespec.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using Kingmaker;
+using Kingmaker.EntitySystem.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrestigeMinus
+{
+    [HarmonyPatch(typeof(UnitEntityData), nameof(UnitEntityData.MarkAsCloneOfMainCharacter))]
+    internal class FixRespec
+    {
+        static void Postfix(ref UnitEntityData __instance)
+        {
+            try
+            {
+                var kc = Game.Instance.Player.MainCharacter.Value;
+                var part = kc.Ensure<UnitPartExpCalculator>();
+                var part2 = __instance.Ensure<UnitPartExpCalculator>();
+                part2.Realexp = part.Realexp;
+                part2.Partysize = part.Partysize;
+            }
+            catch (Exception e) { Main.Logger.Error("Failed to FixRespec", e); }
+        }
+    }
+}
