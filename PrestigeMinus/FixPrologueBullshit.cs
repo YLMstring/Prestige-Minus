47f30fada68a2bfe96abc95256de6d24aa4babcc
YLMstring <61271096+YLMstring@users.noreply.github.com>
2024-08-28 22:07:15 +0800

    Update FixPrologueBullshit.cs

--- PrestigeMinus/FixPrologueBullshit.cs
+++ PrestigeMinus/FixPrologueBullshit.cs
@@ -8,6 +8,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.JsonSystem;
+using Kingmaker.ElementsSystem;
+using Kingmaker.EntitySystem.Persistence.Versioning;
+using Kingmaker.UnitLogic.Parts;
+using Kingmaker.Designers.EventConditionActionSystem.Evaluators;
+using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace PrestigeMinus
 {
@@ -43,6 +51,10 @@ namespace PrestigeMinus
                 {
                     return;
                 }
+                if (__result != null)
+                {
+                    return;
+                }
                 var ins = __instance;
                 if (Game.Instance.Player.RemoteCompanions.Any(p => p.Blueprint == ins.Blueprint))
                 {
@@ -50,6 +62,31 @@ namespace PrestigeMinus
                 }
             }
             catch (Exception e) { Main.Logger.Error("Failed to FixPrologueBullshit2", e); }
+        }
+    }
+
+    [HarmonyPatch(typeof(CompanionInParty), nameof(CompanionInParty.GetValueInternal))]
+    internal class FixPrologueBullshit3
+    {
+        static void Postfix(ref UnitEntityData __result, ref CompanionInParty __instance)
+        {
+            try
+            {
+                if (Game.Instance.Player.Chapter > 0)
+                {
+                    return;
+                }
+                if (__result != null)
+                {
+                    return;
+                }
+                var ins = __instance;
+                if (Game.Instance.Player.RemoteCompanions.Any(p => p.Blueprint == ins.m_Companion?.Get()))
+                {
+                    __result = Game.Instance.Player.MainCharacter;
+                }
+            }
+            catch (Exception e) { Main.Logger.Error("Failed to FixPrologueBullshit3", e); }
         }
     }
 }
